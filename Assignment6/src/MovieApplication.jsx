a9e552a8c93e218c87c67236bd17ac2e4e8d04bf
Alexander Giheden <128364314+gihedenalexander@users.noreply.github.com>
2025-05-20 12:00:42 +0200

    Lade till skelett för metoderna som behövs för att lägga till filmer, ta bort filmer, sortera alfabetisk och sortera 1-5

--- Assignment6/src/MovieApplication.jsx
+++ Assignment6/src/MovieApplication.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react'
+import AddMovieForm from './Movie/AddMovieForm.jsx'
+import Movies from './Movie/Movies.jsx'
+import OrderByAlphaButton from './Movie/OrderByAlphaButton.jsx'
+import OrderByGradeButton from './Movie/OrderByGradeButton.jsx'
+
 function MovieApplication() {
     const [movies, setMovies] = useState([]);
+
+    const addMovie = (title, grade) => {
+
+    }
+
+    const deleteMovie = (movieToDelete) => {
+        
+    }
+
+    const sortByTitle = () => {
+        
+    }
+
+    const sortByGrade = () => {
+        
+    }
 
     return(
         <div className="container">
             <h1>Min filmlista</h1>
 
-            <AddMovieForm/>
+            <AddMovieForm onAddMovie={addMovie}/>
 
-            <OrderByAlphaButton/>
-            <OrderByGradeButton/>
+            <OrderByAlphaButton onClick={sortByTitle}/>
+            <OrderByGradeButton onClick={sortByGrade}/>
 
             <hr></hr>
             <h2>Filmer</h2>
-            <Movies/>
+            <Movies movies={movies} onDelete={deleteMovie}/>
         </div>
     );
 }
